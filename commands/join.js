75382c8695d1ae6dd8a14b11f8329cd63da8c854
leavism <ghuydang@gmail.com>
2018-01-27 21:24:30 -0800

    Readjust permissions

--- commands/join.js
+++ commands/join.js
@@ -22,7 +22,7 @@ exports.conf = {
   enabled: true,
   guildOnly: true,
   aliases: [],
-  permLevel: "User"
+  permLevel: "Simbian"
 };
 
 exports.help = {
--- commands/leave.js
+++ commands/leave.js
@@ -21,7 +21,7 @@ exports.conf = {
   enabled: true,
   guildOnly: true,
   aliases: [],
-  permLevel: "User"
+  permLevel: "Simbian"
 };
 
 exports.help = {
