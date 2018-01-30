57964834c9ecd6095053ededbbf29c3f021cdf96
leavism <ghuydang@gmail.com>
2018-01-30 14:29:00 -0800

    Fixed spelling

--- commands/shipbuildsedit.js
+++ commands/shipbuildsedit.js
@@ -5,17 +5,17 @@ exports.run = async (aclient, message, [action, command, ...value], level) => {
     if (action === "add"){
         ccmd[command] = value.join(" ");
         fs.writeFileSync("./data/shipbuilds/sbuilds.json", JSON.stringify(ccmd), (err) => console.log(err));
-        return message.channel.send(`New custom command ${command} has been set to:\n${ccmd[command]}`);
+        return message.channel.send(`New shipbuild \`\`${command}\`\` has been set to:\n${ccmd[command]}`);
     } else
     if (action === "edit"){
         ccmd[command] = value.join(" ");
         fs.writeFileSync("./data/shipbuilds/sbuilds.json", JSON.stringify(ccmd), (err) => console.log(err));
-        return message.channel.send(`Custom command ${command} has now been set to:\n${ccmd[command]}`);
+        return message.channel.send(`Shipbuild \`\`${command}\`\` has now been set to:\n${ccmd[command]}`);
     } else
     if (action === "remove"){
         delete ccmd[command]
         fs.writeFileSync("./data/shipbuilds/sbuilds.json", JSON.stringify(ccmd), (err) => console.log(err));
-        return message.channel.send(`The ${command} custom command has been deleted.`);
+        return message.channel.send(`The \`\`${command}\`\` shipbuild has been deleted.`);
     } else {
         return message.channel.send(Object.getOwnPropertyNames(ccmd).sort(), {code : "JSON"})
     }
