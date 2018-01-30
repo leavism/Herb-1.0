32ccd708afe8e39b2d83147ffe6e883bf53218c8
leavism <ghuydang@gmail.com>
2018-01-29 19:07:50 -0800

    Don't need it to look ugly

--- commands/stats.js
+++ commands/stats.js
@@ -11,7 +11,7 @@ exports.run = (client, message, args, level) => { // eslint-disable-line no-unus
 • Servers    :: ${client.guilds.size.toLocaleString()}
 • Channels   :: ${client.channels.size.toLocaleString()}
 • Discord.js :: v${version}
-• Node       :: ${process.version}`, {code: "asciidoc"});
+• Node       :: ${process.version}`);
 };
 
 exports.conf = {
