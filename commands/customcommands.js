4019b5b23d3f7dc50404feaed66812418d69e98e
leavism <ghuydang@gmail.com>
2018-02-10 12:02:27 -0800

    Removed useless lines

--- commands/customcommands.js
+++ commands/customcommands.js
@@ -1,8 +1,12 @@
 const ccmd = require("../data/customcommands/ccommands.json");
 const fs = require("fs");
 
-exports.run = async (aclient, message, args, level) => {
-    return message.channel.send(Object.getOwnPropertyNames(ccmd).sort(), {code : "JSON"})
+exports.run = async (client, message, args, level) => {
+    const settings = message.guild
+        ? client.settings.get(message.guild.id)
+        : client.config.defaultSettings;
+
+    return message.channel.send(`List of custom commands. To use them, precede each of the following terms with our command prefix.\n\n${settings.prefix}` + Object.getOwnPropertyNames(ccmd).sort().join(`\n${settings.prefix}`) + "\n\nTo get your own, buy it with Simbits from our Simbit store! Use the ``shop list`` command or ask an Admin for more details.", {code : "JSON"})
 }
 
 exports.conf = {
--- commands/customcommandsedit.js
+++ commands/customcommandsedit.js
@@ -1,7 +1,7 @@
 const ccmd = require("../data/customcommands/ccommands.json");
 const fs = require("fs");
 
-exports.run = async (aclient, message, [action, command, ...value], level) => {
+exports.run = async (client, message, [action, command, ...value], level) => {
     if (action === "add"){
         ccmd[command] = value.join(" ");
         fs.writeFileSync("./data/customcommands/ccommands.json", JSON.stringify(ccmd), (err) => console.log(err));
@@ -17,7 +17,11 @@ exports.run = async (aclient, message, [action, command, ...value], level) => {
         fs.writeFileSync("./data/customcommands/ccommands.json", JSON.stringify(ccmd), (err) => console.log(err));
         return message.channel.send(`The ${command} custom command has been deleted.`);
     } else {
-        return message.channel.send(Object.getOwnPropertyNames(ccmd).sort(), {code : "JSON"})
+        const settings = message.guild
+            ? client.settings.get(message.guild.id)
+            : client.config.defaultSettings;
+
+        return message.channel.send(`List of custom commands. To use them, precede each of the following terms with our command prefix.\n\n${settings.prefix}` + Object.getOwnPropertyNames(ccmd).sort().join(`\n${settings.prefix}`) + "\n\nTo get your own, buy it with Simbits from our Simbit store! Use the ``shop list`` command or ask an Admin for more details.", {code : "JSON"})
     }
 }
 
