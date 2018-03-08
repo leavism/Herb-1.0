f7c21d51ddf38d54a40a39a412c8ab9274f1b864
Leavism <ghuydang@gmail.com>
2018-03-08 13:31:26 -0800

    Phrasing error fixed

--- commands/customcommandsedit.js
+++ commands/customcommandsedit.js
@@ -11,13 +11,15 @@ exports.run = async (client, message, [action, command, ...value], level) => {
     } else
     if (action === "edit"){
         if (command === undefined) return message.reply("Specify which command you'd like to edit, then the new content for its response. For example ``?cce edit sam That was such a bad pun`` will change the response for the ``?sam?`` to \"That was such a bad pun\"");
+        if (ccmd[command] === undefined) return message.reply("That custom command does not exist!");
         if (value.join().length < 1) return message.reply("Specify the new response for your command. For example ``?cce edit sam That was such a bad pun`` will add the custom command ``?sam`` and the response will be \"That was such a bad pun\"");
         ccmd[command] = value.join(" ");
         fs.writeFileSync("./data/customcommands/ccommands.json", JSON.stringify(ccmd), (err) => console.log(err));
         return message.channel.send(`Custom command ${command} has now been set to:\n${ccmd[command]}`);
     } else
     if (action === "remove"){
-        if (command === undefined) return message.reply("Specify which command you'd like to edit, then the new content for its response. For example ``?cce edit sam That was such a bad pun`` will change the response for the ``?sam?`` to \"That was such a bad pun\"");
+        if (ccmd[command] === undefined) return message.reply("That custom command does not exist!");
+        if (command === undefined) return message.reply("Specify which command you'd like to remove. For example ``?cce remove ree`` will remove the ``?ree`` custom command.");
         delete ccmd[command]
         fs.writeFileSync("./data/customcommands/ccommands.json", JSON.stringify(ccmd), (err) => console.log(err));
         return message.channel.send(`The ${command} custom command has been deleted.`);
