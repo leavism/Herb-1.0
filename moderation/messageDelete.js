7f779db1dcfc8238b5ea96dcc5a0ce408eae3798
Leavism <ghuydang@gmail.com>
2018-03-10 08:05:30 -0800

    Doesn't check for bot deletes

--- moderation/messageDelete.js
+++ moderation/messageDelete.js
@@ -4,6 +4,8 @@ module.exports = (client, message) => {
     ? client.settings.get(message.guild.id)
     : client.config.defaultSettings;
 
+    if (message.author.bot) return;
+
     const modLogC = message.guild.channels.find("name", settings.modLogChannel);
     if(message.content.length >= 1900){
         modLogC.send(`${message.author} deleted their message:\n\n`)
