32885c0529eb251211bc38c3d50e6db5e70719d2
Leavism <ghuydang@gmail.com>
2018-03-15 12:08:35 -0700

    Fixed hyphen problem

--- commands/bearing.js
+++ commands/bearing.js
@@ -1,23 +1,21 @@
 exports.run = async(client, message, args) => {
 
     try {
-        let origLat = args[0].replace("_", "-");
-        let origLong = args[1].replace("_","-");
-        let destLat = args[2].replace("_","-");
-        let destLong = args[3].replace("_","-");
+        var origLat = args[0].replace("_", "-");
+        var origLong = args[1].replace("_","-");
+        var destLat = args[2].replace("_","-");
+        var destLong = args[3].replace("_","-");
     } catch (error){
         return message.reply("Please use _ (underscore) in place of the negative sign.\n\nFor example: ``?heading _10 41 _20 290``")
     }
-
-
     
-    if(origLat && origLong && destLat && destLong) {
+    if(origLat && origLong && destLat && destLong && !isNumber([origLat, origLong, destLat, destLong])) {
         let heading = calculateBearing(origLat, origLong, destLat, destLong)
 
         message.channel.send('**Origin Coords**:\nLat: ' + origLat + ' **|** Long: ' + origLong + '\n' + '**Destination Coords**:\nLat: ' + destLat + ' **|** Long: ' + destLong + '\n\n' + 'Set your compass to ' + heading + ' degrees, CMDR!') 
     } else {
         // console.log(origLat)
-        message.reply("Include the origin longitude and latitude, along with the destination longitude and latitude. For example:\n``?heading 20 -41 10 -170``");
+        message.reply("Include the origin longitude and latitude, along with the destination longitude and latitude. Use _ (underscore) in place of negative signs. For example:\n``?heading 20 _41 10 _170``");
     }
 }
 
@@ -57,14 +55,7 @@ function calculateBearing(oLat, oLong, dLat, dLong){
 
 function isNumber(arrayOfNumbers) {
     arrayOfNumbers.forEach(number => {
-        console.log(number)
         if(isNaN(number)) return false;
     })
     return true;
-}
-
-function hasHyphen(arrayOfNumbers){
-    return arrayOfNumbers.forEach( number => {
-        if (number.indexOf("-") > -1) return true;
-    })
 }
