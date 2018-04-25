5336228e884f38fb344ac4100982f101c9b53f33
Leavism <ghuydang@gmail.com>
2018-04-25 10:16:19 -0700

    Fixing crash attempt fix #2

--- moderation/message.js
+++ moderation/message.js
@@ -2,16 +2,16 @@ const Discord = require("discord.js");
 
 module.exports = (client, message) => {
 
+    const settings = message.guild
+    ? client.settings.get(message.guild.id)
+    : client.config.defaultSettings;
+    let admin = message.guild.roles.find(r => r.name.toLowerCase() === settings.adminRole.toLowerCase());
+    const modLogC = message.guild.channels.find("name", settings.modLogChannel);
+
     if(!message.guild) return;
     if(message.author.bot) return;
     if(message.member.roles.has(admin.id)) return;
 
-    const settings = message.guild
-    ? client.settings.get(message.guild.id)
-    : client.config.defaultSettings;
-    
-    let admin = message.guild.roles.find(r => r.name.toLowerCase() === settings.adminRole.toLowerCase());
-    const modLogC = message.guild.channels.find("name", settings.modLogChannel);
 
     //Repeated content filter
     let counter = 0;
