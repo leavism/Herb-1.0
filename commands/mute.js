269174871f7ff71c2db1f80c76cfd5f178c1e756
Leavism <ghuydang@gmail.com>
2018-04-29 09:31:37 -0700

    Mute/unmute commands

---
+++ commands/mute.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const muted = require('../data/mute/muted.json');
+
+exports.run = async (client, message, level) => {
+  if(message.mentions.members.first() === undefined){
+    return message.reply("Please mention who you're muting. For example: ``?mute @Leavism2011``")
+  }
+  var target = message.mentions.members.first();
+  let rolesStringArry = target.roles.map(m => m.name).filter(m => m != '@everyone')
+
+  muted[target.id] = rolesStringArry;
+  fs.writeFileSync('./data/mute/muted.json', JSON.stringify(muted), (err) => console.log(err));
+
+  let rolesArray = target.roles.array();
+  target.removeRoles(rolesArray);
+
+  if(message.guild.roles.find("name","mute")){
+    target.addRole(message.guild.roles.find("name","mute"))
+    message.channel.send(`${target} has been muted.`)
+  } else {
+    message.reply(`there isn't a Mute role but I've removed ${target}'s roles.`)
+  }
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: 'Moderator'
+};
+
+exports.help = {
+  name: 'mute',
+  category: 'Moderation',
+  description: 'Removes all roles and add the Mute role to someone',
+  usage: '?mute <mention Discord member>',
+  example: ["?mute @Leavism#2011"]
+};
---
+++ commands/unmute.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const muted = require('../data/mute/muted.json');
+
+exports.run = async (client, message, level) => {
+  if(message.mentions.members.first() === undefined){
+    return message.reply("Please mention who you're unmuting. For example: ``?unmute @Leavism2011``")
+  }
+  var target = message.mentions.members.first();
+
+  if(muted[target.id]){ 
+    var addRoles = [];
+    muted[target.id].forEach(role => {
+      addRoles.push(message.guild.roles.find("name", role));
+    })
+  
+    target.addRoles(addRoles);
+
+    delete muted[target.id];
+    fs.writeFileSync('./data/mute/muted.json', JSON.stringify(muted), (err) => console.log(err));
+  
+    message.channel.send(`${target} has been unmuted and gained their roles back.`);
+  } else {
+    return message.reply("That person has not been muted with the bot.");
+  }
+
+  
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: 'Moderator'
+};
+
+exports.help = {
+  name: 'unmute',
+  category: 'Moderation',
+  description: 'Removes all roles and add the Mute role to someone',
+  usage: '?unmute <mention Discord member>',
+  example: ["?unmute @Leavism#2011"]
+};
