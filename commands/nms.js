a2e86004256dc241a9d26b1e28a01722d7fd25fe
Brandon Evers <brandon.evers@uptake.com>
2018-08-05 03:19:08 -0500

    Updated all recruit commands

--- commands/nms.js
+++ commands/nms.js
@@ -5,16 +5,17 @@ exports.run = async(client, message, [member]) => {
         member = message.mentions.members.first();
     }
 
+    const general = message.guild.channels.find("name", "general");
     let recruit = message.guild.roles.find('name', 'Recruit');
     let nms = message.guild.roles.find('name', "No Man's Sky");
     let simbian = message.guild.roles.find('name', 'Simbian');
 
     var msg = `You're now a member of Simbad, ${member}! This grants access to our text and voice channels, so feel free to get to know `+
     `everyone. You've also been given the Recruit role, which indiciates you're a newer member. Once you've grown into our community, you can lose `+
-    `the Recruit role and celebrate with dank memes.`
+    `the Recruit role and celebrate with dank memes. Everyone, say hello!`
 
     await member.addRoles([recruit, nms, simbian]).catch(err => { msg = `${member} already has those roles. That's right, no more eternal recruits!`})
-    message.channel.send(msg);
+    general.send(msg);
 }
 
 exports.conf = {
