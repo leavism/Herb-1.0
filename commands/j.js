7289d93ec1be371ffeefe6b7e14cc93dd0f3de9f
bevers222 <bevers222@gmail.com>
2018-08-31 20:59:16 -0500

    done

--- commands/j.js
+++ commands/j.js
@@ -1,11 +1,11 @@
 exports.run = async (client, message, args) => {
   let currentUser = message.author.id;
   if (currentUser == 295411971262644224) {
-    message.guild.members.get(295411971262644224).kick("You used the command");
-    message.channel.send("I have kicked Anwi for using his command")
+    //message.guild.members.get(295411971262644224).kick("You used the command");
+    message.channel.send("Im going to kick you for using his command, <@295411971262644224>")
   }
   else {
-    message.channel.send('Anwidoxo#0476 has left. Hope he had a nice stay. RIP :rip:')
+    message.channel.send('Anwidoxo#0476 has left. Hope <@295411971262644224> had a nice stay. RIP <:rip:353912065712324618>')
   }
 };
 
