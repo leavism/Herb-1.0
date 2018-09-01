a1d958925d483984bcd411170d78e1e0eb1e7545
bevers222 <bevers222@gmail.com>
2018-08-31 20:47:42 -0500

    alala

--- commands/j.js
+++ commands/j.js
@@ -1,9 +1,10 @@
 exports.run = async (client, message, args) => {
-  let currentUser = client.user.id;
+  let currentUser = message.author.id;
   console.log(currentUser);
   // let user = message.guild.members.random();
   // message.channel.send(`kicking ${user}`);
 };
+454845440408420353
 
 exports.conf = {
   enabled: true,
@@ -13,7 +14,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: "f",
+  name: "j",
   category: "Server",
   description: "Try it and find out",
   usage: "?f",
