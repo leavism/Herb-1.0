e559785c957fa6542997d39768ed91683c8166f1
bevers222 <bevers222@gmail.com>
2018-08-31 21:09:18 -0500

    dlddl

--- commands/f.js
+++ commands/f.js
@@ -1,20 +1,20 @@
 exports.run = async (client, message, args) => {
-  let currentUser = message.author.id;
+  let currentUser = message.author.id
   if (currentUser == 295411971262644224) {
-    //message.guild.members.get(295411971262644224).kick("You used the command");
+    //message.guild.members.get(295411971262644224).kick("You used the command")
     message.channel.send("Im going to kick you for using this command, STOP. <@295411971262644224>")
   }
   else {
     message.channel.send('Anwidoxo#0476 has left. Hope <@295411971262644224> had a nice stay. RIP <:rip:353912065712324618>')
   }
-};
+}
 
 exports.conf = {
   enabled: true,
   guildOnly: true,
   aliases: [],
-  permLevel: "Simbian"
-};
+  permLevel: "User"
+}
 
 exports.help = {
   name: "f",
@@ -22,4 +22,4 @@ exports.help = {
   description: "Try it and find out",
   usage: "?f",
   example: ["?f"]
-};
+}
