392404d4548bcc74b4ce6e9dc7fc0aad4697cc28
bevers222 <bevers222@gmail.com>
2018-09-02 05:04:20 -0500

    Added Budget

---
+++ commands/budget.js
@@ -0,0 +1,33 @@
+const shopCommon = require('./commonShop.js')
+const data = require("../data/shop/data.json");
+const cTable = require('console.table');
+const fs = require("fs");
+const Discord = require("discord.js")
+
+exports.run = async (client, message, args) => {
+    let given;
+    data.users.forEach(user => {
+        given += user.balance;
+    })
+
+    message.channel.send(`The budget is at ${data.budget} Simbits.\nThe total Simbits given to users is at ${given} Simbits.`)
+}
+
+exports.conf = {
+    enabled: true,
+    guildOnly: true,
+    aliases: [],
+    permLevel: "Administrator"
+}
+
+exports.help = {
+    name: "budget",
+    category: "Shop",
+    description: "Shop command to see the Simbits budget",
+    usage: "?budget",
+    example: ["?budget"]
+}
+
+
+
+
