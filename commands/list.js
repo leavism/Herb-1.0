1cdb6903f9ecd215a2ff5848531f61dc5b52e28d
bevers222 <bevers222@gmail.com>
2018-09-07 13:04:19 -0500

    Added list because reasons

---
+++ commands/list.js
@@ -0,0 +1,18 @@
+exports.run = async (client, message, args) => {
+    message.channel.send("BEVERS' LIST:\n ```\n1. JayDonks\n 2. Jrose\n 3. Moist Accident\n 4. Havoc\n 5. SpInFuZoR\n 6. PlsNoBuli");
+}
+
+exports.conf = {
+    enabled: true,
+    guildOnly: true,
+    aliases: [],
+    permLevel: "Bot Owner"
+};
+
+exports.help = {
+    name: "list",
+    category: "Server",
+    description: "Bevers' list.",
+    usage: "?list",
+    example: ["?list"]
+};
