636ecadb0798d1b709b2e231eed4ba2edd7016c6
bevers222 <bevers222@gmail.com>
2018-10-04 19:40:35 -0500

    Added anwi to list

--- commands/list.js
+++ commands/list.js
@@ -1,5 +1,5 @@
 exports.run = async (client, message, args) => {
-    message.channel.send("BEVERS' LIST: ```md\n1. JayDonks\n2. JRose\n3. Moist Accident\n4. Havoc\n5. SpInFuZoR\n6. PlsNoBuli\n7. Madporg Horizon\n8. Provalion```");
+    message.channel.send("BEVERS' LIST: ```md\n1. JayDonks\n2. JRose\n3. Moist Accident\n4. Havoc\n5. SpInFuZoR\n6. PlsNoBuli\n7. Madporg Horizon\n8. Provalion \n9. Dr Anwid```");
 }
 
 exports.conf = {
