53dbbd5d5db27ef741953860c2c7f5d7b891e3c7
bevers222 <bevers222@gmail.com>
2018-12-05 11:40:13 -0600

    test

--- commands/face.js
+++ commands/face.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 exports.run = async (client, message, args) => {
   message.channel.send('༼ つ ◕_◕ ༽つ');
   client.channels.get("125498072661753856").fetchMessages({ limit: 100 })
-    .then(message => fs.writeFileSync("../data/l.txt", `${message.timestamp} - ${message.author}: ${message.content}`, (err) => console.log(err)))
+    .then(message => fs.writeFileSync("./data/l.txt", `${message.timestamp} - ${message.author}: ${message.content}`, (err) => console.log(err)))
     .catch(console.error)
 }
 
